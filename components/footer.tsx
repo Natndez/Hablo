9ec4a7c892a7b35192f7c1e41be66d939d3fcc1e
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-08-11 21:30:54 -0400

    Set up route structure for learn page

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,16 +7,6 @@ export const Footer = () => {
             <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
                 <Button size="lg" variant="ghost" className="w-full">
                     <Image
-                        src="/temp/it.svg" 
-                        alt="Croatian" 
-                        height={32} 
-                        width={40}
-                        className="mr-4 rounded-md"
-                    />
-                    Italian
-                </Button>
-                <Button size="lg" variant="ghost" className="w-full">
-                    <Image
                         src="/temp/es.svg" 
                         alt="Croatian" 
                         height={32} 
@@ -24,6 +14,16 @@ export const Footer = () => {
                         className="mr-4 rounded-md"
                     />
                     Spanish
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image
+                        src="/temp/it.svg" 
+                        alt="Croatian" 
+                        height={32} 
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Italian
                 </Button>
                 <Button size="lg" variant="ghost" className="w-full">
                     <Image
