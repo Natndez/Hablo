4645d77b75617e0efebf457a9bfa1b64e56f296e
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-08-11 23:41:43 -0400

    Added UserProgress skeleton to stickywrapper

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,12 +1,19 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+
 import { Header } from "@/app/(main)/learn/header";
 
 const LearnPage = () => {
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
-                My sticky sidebar
+                <UserProgress 
+                    activeCourse={{ title: "Spanish", imageSrc: "/temp/es.svg" }}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
             </StickyWrapper>
             <FeedWrapper>
                 <Header title="Spanish" />
