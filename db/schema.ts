a9c1cc6e1d91734cbc5712ac31dcf420df87bb19
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-08-12 20:01:42 -0400

    Added new db tables and relations

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,7 @@
 // DB schema
 
-import { pgTable, serial, text } from "drizzle-orm/pg-core"
+import { relations } from "drizzle-orm";
+import { pgTable, serial, text, integer } from "drizzle-orm/pg-core"
 
 // Courses Table
 export const courses = pgTable("courses", {
@@ -8,3 +9,26 @@ export const courses = pgTable("courses", {
     title: text("title").notNull(), // notNull() means its required
     imageSrc: text("image_src").notNull(),
 });
+
+// Relation between courses (many to many)
+export const coursesRelations = relations(courses, ({ many }) => ({
+    userProgress: many(userProgress),
+}));
+
+// User progress table 
+export const userProgress = pgTable("user_progress", {
+    userId: text("user_id").primaryKey(),
+    userName: text("user_name").notNull().default("User"),
+    userImgSrc: text("user_image_src").notNull().default("/temp/mascot.svg"),
+    activeCourseId: integer("active_course_id").references(() => courses.id, { onDelete: "cascade" }),
+    hearts: integer("hearts").notNull().default(5),
+    points: integer("hearts").notNull().default(0),
+});
+
+// Relation between user progress (one to many)
+export const userProgressRelations = relations(userProgress, ({ one }) => ({
+    activeCourse: one(courses, {
+        fields: [userProgress.activeCourseId],
+        references: [courses.id],
+    })
+}))
