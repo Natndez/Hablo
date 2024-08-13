55c2eefbd02716d4a24a4230b231bd6d5759eabb
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-08-12 20:07:53 -0400

    Created query to get user progress

--- db/queries.ts
+++ db/queries.ts
@@ -1,6 +1,30 @@
 import { cache } from "react";
 import db from "@/db/drizzle"
+import { auth } from "@clerk/nextjs/server";
+import { userProgress } from "./schema";
+import { eq } from "drizzle-orm";
 
+// Query to get user progress
+export const getUserProgress = cache(async () => {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return null;
+    }
+
+    // Actual query
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true,
+        },
+    });
+
+    return data;
+});
+
+
+// Query to get course
 // Using cache so we dont have to query db everytime
 export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
