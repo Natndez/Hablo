b2956e899f8eddaeb5a5954909d614190b288028
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-08-16 16:24:44 -0400

    Added new button variant and made progress on lesson button

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -73,10 +73,40 @@ export const LessonButton = ({
                     marginTop: isFirst && !isCompleted ? 60 : 24
                 }}
             >
-                {/* If current return the following... (havent seen this syntax before) */}
                 {current ? (
-                    <div>
-                        Current
+                    // If Current lesson return the following (little badge above to indicate where to go)
+                    <div className="h-[102px] w-[102px] relative">
+                        <div className="absolute -top-6 left-2.5 px-3 py-2.5 border-2 font-bold uppercase text-green-500 bg-white rounded-xl animate-bounce tracking-wide z-10">
+                            Begin
+                            <div 
+                                className="absolute left-1/2 -bottom-2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-x-1/2"
+                            />
+                        </div>
+                        <CircularProgressbarWithChildren
+                            value={Number.isNaN(percentage) ? 0 : percentage}
+                            styles={{
+                                path: {
+                                    stroke: "#4ade80",
+                                    },
+                                trail: {
+                                    stroke: "#e5e7eb",
+                                },
+                            }}
+                        >
+                            <Button
+                                size="rounded"
+                                variant={locked ? "locked" : "secondary"}
+                                className="h-[70px] w-[70px] border-b-8"
+                            >
+                                <Icon 
+                                    className={cn(
+                                        "h-10 w-10",
+                                        locked ? "fill-neutral-400 text-neutral-400 stroke-neutral-400" : "fill-primary-foreground text-primary-foreground",
+                                        isCompleted && "fill-none stroke-[4]"
+                                    )}
+                                />
+                            </Button>
+                        </CircularProgressbarWithChildren>
                     </div>
                 ) : (
                     <div>
