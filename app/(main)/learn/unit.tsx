600aa2645092c5a2440cbaf931bae30fce4cb773
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-08-15 20:41:53 -0400

    Began work on lesson button component

--- app/(main)/learn/unit.tsx
+++ app/(main)/learn/unit.tsx
@@ -2,6 +2,7 @@
 import { lessons, units } from "@/db/schema";
 import { boolean } from "drizzle-orm/mysql-core";
 import { UnitBanner } from "./unit-banner";
+import { LessonButton } from "./lesson-button";
 
 type Props = {
     id: number;
@@ -30,6 +31,25 @@ export const Unit = ({
     return (
         <>
             <UnitBanner title={title} description={description} />
+            <div className="flex items-center flex-col relative">
+                {lessons.map((lesson, index) => {
+                    const isCurrent = lesson.id === activeLesson?.id;
+                    const isLocked = !lesson.completed && !isCurrent; // Makes sure the right lessons are unlocked
+
+                    return (
+                        <LessonButton 
+                            key={lesson.id}
+                            id={lesson.id}
+                            index={index}
+                            totalCount={lessons.length - 1}
+                            current={isCurrent}
+                            locked={isLocked}
+                            percentage={activeLessonPercentage}
+                        />
+                    );
+                })}
+
+            </div>
         </>
     );
 }
