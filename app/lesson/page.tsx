49277e534ce7d94f1be2ca32b0f18debb1ac480a
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-08-18 15:05:53 -0400

    Added header and quiz components to lesson page

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -1,7 +1,40 @@
-const LessonPage = () => {
+import { getLesson, getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+import { Quiz } from "./quiz";
+
+const LessonPage = async () => {
+    // Data needed from db
+    const lessonData = getLesson();
+    const userProgressData = getUserProgress();
+
+    // Using promise to get lesson and userProgress
+    const [
+        lesson,
+        userProgress,
+    ] = await Promise.all([
+        lessonData,
+        userProgressData,
+    ]);
+
+    // Without the following, can't be on this page
+    if (!lesson || !userProgress) {
+        redirect("/learn");
+    }
+
+    // Lesson's initial percentage when beginning session
+    const initialPercent = lesson.challenges
+        .filter((challenge) => challenge.completed)
+        .length / lesson.challenges.length * 100;
+
     return (
         <div>
-            Lessons!
+            <Quiz 
+                initialLessonId={lesson.id}
+                initialLessonChallenges={lesson.challenges}
+                initialHearts={userProgress.hearts}
+                initialPercentage={initialPercent}
+                userSubscription={null} // Add soon
+            />
         </div>
     );
 };
