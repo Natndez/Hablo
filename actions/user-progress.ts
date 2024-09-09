11c8db344f50ca7805413df2496c46921543db4c
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-09-09 14:33:54 -0400

    Finished reduceHearts server action

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -2,7 +2,7 @@
 
 import db from "@/db/drizzle";
 import { getCourseById, getUserProgress } from "@/db/queries";
-import { challengeProgress, userProgress } from "@/db/schema";
+import { challengeProgress, challenges, userProgress } from "@/db/schema";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
@@ -72,6 +72,16 @@ export const reduceHearts = async (challengeId: number) => {
     const currentUserProgress = await getUserProgress();
     // TODO: get user subscription
 
+    const challenge = await db.query.challenges.findFirst({
+        where: eq(challenges.id, challengeId)
+    });
+
+    if(!challenge) {
+        throw new Error("Challenge not found");
+    }
+
+    const lessonId = challenge.lessonId;
+
     // Using this to check for exisitng challenge progress instead of our front-end method
     const existingChallengeProgress = await db.query.challengeProgress.findFirst({
         where: and(
@@ -87,4 +97,25 @@ export const reduceHearts = async (challengeId: number) => {
     if(isPractice) {
         return { error: "Practice" }
     }
+
+    if(!currentUserProgress) {
+        throw new Error("User progress not found") // Critical error thus the diffent type of error compared to above
+    }
+
+    // TODO: Handle subscription
+
+    if(currentUserProgress.hearts === 0) {
+        return { error: "hearts" }; // No hearts so no point in reducing
+    }
+
+    // Updating hearts (either subtract 1 from current amount or 0, hence max)
+    await db.update(userProgress).set({
+        hearts: Math.max(currentUserProgress.hearts - 1, 0),
+    }).where(eq(userProgress.userId, userId));
+
+    revalidatePath("/shop");
+    revalidatePath("/learn");
+    revalidatePath("/quests");
+    revalidatePath("/leaderboard");
+    revalidatePath(`/lesson/${lessonId}`);
 }
